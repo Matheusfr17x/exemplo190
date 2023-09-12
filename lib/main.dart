c1dca0bb99fb533f2368ce3b0797566043f8e810
Matheusfr17x <61791461+Matheusfr17x@users.noreply.github.com>
2023-09-12 06:12:36 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,86 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Fontes Externas',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        //useMaterial3: true,
+        primaryColor: Color.fromRGBO(82, 12, 12, 1),
+        // Outras configurações do tema
+        // ...
       ),
-      home: Center(
-        child: Text(
-          'Olá Mundos',
-          style: TextStyle(
-              fontSize: 80,
-              fontWeight: FontWeight.bold,
-              color: Colors.green,
-              letterSpacing: 8,
-              wordSpacing: 20),
+      home: HelloWorld(),
+    );
+
+
+  }
+}
+class HelloWorld extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+
+            Text(
+              'Olá, esta e uma fonte importada do google!',
+              style: GoogleFonts.adventPro(
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+
+            SizedBox(height: 20),
+            Text(
+              'Olá, esta e uma fonte importada do google!',
+              style: GoogleFonts.montserrat(
+                fontSize: 36.0,
+                fontWeight: FontWeight.normal,
+                textStyle: TextStyle(
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+
+            Text(
+              'Olá, esta e uma fonte importada do google!',
+              style: GoogleFonts.sahitya(
+                fontSize: 36.0,
+                fontWeight: FontWeight.normal,
+                textStyle: TextStyle(
+                  fontStyle: FontStyle.italic,
+                ),
+
+
+              ),
+            ),
+                        SizedBox(height: 20),
+            Text(
+              'Olá, esta e uma fonte importada do google!',
+              style: GoogleFonts.badScript(
+                fontSize: 36.0,
+                fontWeight: FontWeight.normal,
+                textStyle: TextStyle(
+                  fontStyle: FontStyle.normal,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
